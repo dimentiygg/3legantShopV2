ec490f9e27bb510ab5eed2c4453b5340b6286aa6
Dmytro <az.36419.a@gmail.com>
2024-05-15 09:02:27 +0300

    Update product_carousel.js

--- src/js/index/product_carousel.js
+++ src/js/index/product_carousel.js
@@ -19,17 +19,11 @@ carouselList.addEventListener('click', event => {
 
   if (heartButton) {
     const currentHref = svgIcon.getAttribute('href');
-    if (currentHref === './assets/public/symbol-defs.svg#icon-black-heart') {
-      svgIcon.setAttribute(
-        'href',
-        './assets/public/symbol-defs.svg#icon-heart'
-      );
+    if (currentHref === './assets/symbol-defs.svg#icon-black-heart') {
+      svgIcon.setAttribute('href', './assets/symbol-defs.svg#icon-heart');
       svgIcon.closest('svg').style.fill = '#6c7275';
     } else {
-      svgIcon.setAttribute(
-        'href',
-        './assets/public/symbol-defs.svg#icon-black-heart'
-      );
+      svgIcon.setAttribute('href', './assets/symbol-defs.svg#icon-black-heart');
       svgIcon.closest('svg').style.fill = 'black';
     }
   }
